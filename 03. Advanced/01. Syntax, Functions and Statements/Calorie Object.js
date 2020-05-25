b01ca658c7d06509336152cfa1ffb1b00c17ff2a
pmg23_b.kondev <boriskondev@gmail.com>
2020-05-25 10:20:13 +0300

    25.05.2020

--- 03. Advanced/01. Syntax, Functions and Statements/Calorie Object.js
+++ 03. Advanced/01. Syntax, Functions and Statements/Calorie Object.js
@@ -1,23 +1,13 @@
 function solve(arr) {
-    let keyValuePair;
+    let obj = {};
     let key;
     let value;
-    let keyValuePairsArr = [];
-    let newArr = []
-    for (let i = 0; i <= arr.length; i+=2) {
-        keyValuePair = arr.slice(i, i+2);
-        [key, value] = keyValuePair;
-        value = Number(value);
-        if (keyValuePair.length > 0) {
-            keyValuePairsArr.push(key);
-            keyValuePairsArr.push(value)
-            newArr.push(keyValuePairsArr)
-            keyValuePairsArr = []
-        }
+    for (let i = 0; i < arr.length; i+=2) {
+        key = arr[i];
+        value = arr[i+1];
+        obj[key] = Number(value);
     }
-    let arrToMap = new Map(newArr);
-    let mapToObj = Object.fromEntries(arrToMap);
-    console.log(mapToObj)
+    console.log(obj)
 }
 
 solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])
--- 03. Advanced/01. Syntax, Functions and Statements/Not Solved - Time to Walk.js
+++
@@ -1,9 +0,0 @@
-function timeToWalk(steps, footprintLength, studentSpeed) {
-    let distanceKilometers = (steps * footprintLength) / 1000;
-    let time = (distanceKilometers / studentSpeed) * 60
-    let additionalTime = Math.floor(distanceKilometers * 1000 / 500);
-    // let totalTime = time + additionalTime;
-    console.log(Math.round(time / 60));
-}
-
-timeToWalk(4000, 0.60, 5);
