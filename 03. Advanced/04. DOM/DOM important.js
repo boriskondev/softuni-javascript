46bdd57290c6d65e705fce8ea1491ab346265b6c
pmg23_b.kondev <boriskondev@gmail.com>
2020-06-04 17:08:41 +0300

    04.06.2020

---
+++ 03. Advanced/04. DOM/DOM important.js
@@ -0,0 +1,9 @@
+// innerHTML - returns the HTML as string;
+// value - for the elements inputed by user;
+// textContent - the elements which are the text of the page
+
+// Control + Shift + J - open console in Chrome
+// Click on an element in Chrome and input $0 in console - gives reference to the given element
+// .childNodes - returns a list on nodes
+
+
