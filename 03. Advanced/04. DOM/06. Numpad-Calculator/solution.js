dc59adb8aad91eb4d877567d8ef1faa1d6015f05
pmg23_b.kondev <boriskondev@gmail.com>
2020-06-07 22:14:20 +0300

    07.06.2020

--- 03. Advanced/04. DOM/06. Numpad-Calculator/solution.js
+++ 03. Advanced/04. DOM/06. Numpad-Calculator/solution.js
@@ -1,4 +1,26 @@
 function solve() {
+    let keyboardButtons = Array.from(document.querySelectorAll("button"));
+    keyboardButtons.forEach(b => b.addEventListener("click", keyboardClicked));
+    
+    let expressionOutput = document.getElementById("expressionOutput");
+    let resultOutput = document.getElementById("resultOutput");
 
-    //TODO...
+    function keyboardClicked(e) {
+        let buttonValue = e.target.value;
+        buttonValue = Number.parseInt(buttonValue)
+
+        if (Number.isNaN(buttonValue) && buttonValue !== ".") {
+            console.log("Not a number!")
+        } else {
+            console.log("Number!")
+        }
+
+
+        // if (buttonValue === "Clear") {
+        //     expressionOutput.textContent = "";
+        //     resultOutput.textContent = "";
+        // } else {
+        //     console.log(Number.isNaN(buttonValue))
+        // }
+    }
 }
