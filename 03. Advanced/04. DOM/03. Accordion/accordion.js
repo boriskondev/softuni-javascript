eec7cbca9499dc20ea566cc695743dc59f8b1ac2
pmg23_b.kondev <boriskondev@gmail.com>
2020-06-07 14:30:15 +0300

    07.06.2020

--- 03. Advanced/04. DOM/03. Accordion/accordion.js
+++ 03. Advanced/04. DOM/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let button = document.getElementsByClassName("button")[0];
+    let additionalContent = document.getElementById("extra");
+
+    if (additionalContent.style.display === "none") {
+        button.textContent = "Less";
+        additionalContent.style.display = "block";
+    } else {
+        button.textContent = "More";
+        additionalContent.style.display = "none";
+    }
 }
--- 03. Advanced/04. DOM/DOM important.js
+++ 03. Advanced/04. DOM/DOM important.js
@@ -2,6 +2,13 @@
 // value - for the elements inputed by user;
 // textContent - the elements which are the text of the page
 
+// ---- Difference between textNode and textContent ----
+// - createTextNode() is a method and works just as its name says: it creates an element... 
+// then you must do something with it (like in your example, where you append it as a child);
+// so it is useful if you want to have a new element and place it somewhere
+// - textContent is a property you may get or set, with a unique statement and nothing else;
+// so it is useful when you only want to change the content of an already existing element
+
 // Control + Shift + J - open console in Chrome
 // Click on an element in Chrome and input $0 in console - gives reference to the given element
 // .childNodes - returns a list on nodes
