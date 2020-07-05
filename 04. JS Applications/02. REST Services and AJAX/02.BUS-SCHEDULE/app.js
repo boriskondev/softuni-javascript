3535e1f8a8c34f768e65882a97405670ffa83d0a
pmg23_b.kondev <boriskondev@gmail.com>
2020-07-05 23:17:18 +0300

    05.07.2020

--- 04. JS Applications/02. REST Services and AJAX/02.BUS-SCHEDULE/app.js
+++ 04. JS Applications/02. REST Services and AJAX/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,31 @@
 function solve() {
+    let infoField = document.querySelector("span.info");
+    let departButton = document.querySelector("input#depart");
+    let arriveButton = document.querySelector("input#arrive");
+
+    let baseUrl = "https://judgetests.firebaseio.com/schedule/";
+    let nextBusStop = "depot";
+    let currentBusStop = ""
 
     function depart() {
-        console.log('Depart TODO...');
+        fetch(baseUrl + `${nextBusStop}.json`)
+            .then((response) => (response.json()))
+            .then((result) => showInfo(result));
+
+        function showInfo(data) {
+            infoField.textContent = `Next stop: ${data.name}`;
+            nextBusStop = data.next;
+            currentBusStop = data.name;
+        }
+
+        departButton.disabled = true;
+        arriveButton.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoField.textContent = `Arriving at: ${currentBusStop}`;
+        departButton.disabled = false;
+        arriveButton.disabled = true;
     }
 
     return {
