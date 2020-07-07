6a8bafaf753280901a02b068f6fe8b258b284fd0
pmg23_b.kondev <boriskondev@gmail.com>
2020-07-08 00:44:33 +0300

    08.07.2020

--- 04. JS Applications/02. REST Services and AJAX/03.PHONEBOOK/app.js
+++ 04. JS Applications/02. REST Services and AJAX/03.PHONEBOOK/app.js
@@ -1,5 +1,54 @@
 function attachEvents() {
-    console.log('TODO...');
+    const loadButton = document.querySelector("#btnLoad");
+    const createButton = document.querySelector("#btnCreate");
+
+    const personId = document.querySelector("#person");
+    const personPhone = document.querySelector("#phone");
+
+    const phonebook = document.querySelector("#phonebook");
+
+    createButton.addEventListener("click", () => {
+        if (personId.value !== "" && personPhone.value !== "") {
+            fetch(baseUrl, {
+                method: "POST",
+                body: JSON.stringify({person: personId.value, phone: personPhone.value})
+            })
+            personId.value = "";
+            personPhone.value = "";
+        }
+    });
+
+    let baseUrl = "https://phonebook-nakov.firebaseio.com/phonebook.json"
+
+    loadButton.addEventListener("click", () => {
+        fetch(baseUrl)
+            .then((response) => (response.json()))
+            .then((result) => (createLiElement(result)));
+    });
+
+    function createLiElement(result) {
+        phonebook.innerHTML = "";
+        if (result === null) {
+            let listItem = document.createElement("li");
+            listItem.setAttribute("id", "phonebook");
+            listItem.textContent = "No result/s found!";
+            listItem.appendChild(liButton);
+            phonebook.appendChild(listItem);
+        } else {
+            for (let key of Object.keys(result)) {
+                let listItem = document.createElement("li");
+                listItem.setAttribute("id", "phonebook");
+                listItem.textContent = `${result[key].person}: ${result[key].phone}`
+                let liButton = document.createElement("button");
+                liButton.textContent = "Delete";
+                liButton.addEventListener("click", () => {
+                    listItem.remove();
+                })
+                listItem.appendChild(liButton);
+                phonebook.appendChild(listItem);
+            }
+        }
+    }
 }
 
 attachEvents();
---
+++ 04. JS Applications/requestImportant.js
@@ -0,0 +1,2 @@
+// https://phonebook-nakov.firebaseio.com/phonebook.json/ - URL to main JSON;
+// https://phonebook-nakov.firebaseio.com/phonebook/{id}.json/ - URL to specific id JSON;
