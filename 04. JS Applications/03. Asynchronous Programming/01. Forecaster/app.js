4af0045de9465ebc0cfc48a62982cdcd24fb7d27
pmg23_b.kondev <boriskondev@gmail.com>
2020-07-12 14:07:22 +0300

    12.07.2020

--- 04. JS Applications/03. Asynchronous Programming/01. Forecaster/app.js
+++ 04. JS Applications/03. Asynchronous Programming/01. Forecaster/app.js
@@ -1,9 +1,6 @@
 import el from "./dom.js";
 import * as data from "./data.js";
 
-// async казва "Тази функция е асинхронна и ще върне Promise;
-// await казва "Вземи резултата от Promise-a, когато е готов;
-
 const symbols = {
     "Sunny": "&#x2600;",
     "Partly sunny": "&#x26C5;",
@@ -21,50 +18,56 @@ window.addEventListener("load", () => {
     document.querySelector("#submit").addEventListener("click", getForecast);
 
     async function getForecast() {
-        mainDiv.style.display = "";
 
         const locationName = input.value.toLowerCase();
         const code = await data.getCodeAsync(locationName);
 
-        const todayRes = data.getTodayAsync(code);
-        const upcomingRes = data.getUpcomingAsync(code);
+        if (code === undefined || code === "") {
+            input.value = "Location not found! Try again.";
+            if (mainDiv.style.display === "block") {
+                mainDiv.style.display = "none";
+            }
+        } else {
+            const today = await data.getTodayAsync(code);
+            const upcoming = await data.getUpcomingAsync(code);
 
-        const [today, upcoming] = [ await todayRes, await upcomingRes ];
+            const symbolSpan = el("span", "", {className: "condition symbol"});
+            symbolSpan.innerHTML = symbols[today.forecast.condition];
 
-        const symbolSpan = el("span", "", {className: "condition symbol"});
-        symbolSpan.innerHTML = symbols[today.forecast.condition];
-
-        const tempSpan = el("span", "", {className: "forecast-data"});
-        tempSpan.innerHTML = `${today.forecast.low}${symbols.Degrees}/${today.forecast.high}${symbols.Degrees}`;
-
-        todayDiv.appendChild(el("div", [
-            symbolSpan,
-            el("span", [
-                el("span", today.name, {className: "forecast-data"}),
-                tempSpan,
-                el("span", today.forecast.condition, {className: "forecast-data"})
-                ], {className: "condition"}),
-        ], {className: "forecast"}));
-
-        const forecastInfo = el("div", upcoming.forecast.map(renderUpcoming), {className: "forecast-info"});
-        upcomingDiv.appendChild(forecastInfo);
-
-        function renderUpcoming(forecast) {
-            const symbolSpan = el("span", "", {className: "symbol"});
-            symbolSpan.innerHTML = symbols[forecast.condition];
-
-            const tempSpan = el("span", "", {className: "forecast-data"})
+            const tempSpan = el("span", "", {className: "forecast-data"});
             tempSpan.innerHTML = `${today.forecast.low}${symbols.Degrees}/${today.forecast.high}${symbols.Degrees}`;
 
-
-            const result = el("span", [
+            todayDiv.innerHTML = "";
+            todayDiv.appendChild(el("div", [
                 symbolSpan,
-                tempSpan,
-                el("span", forecast.condition, {className: "forecast-data"})
-            ], {className: "upcoming"});
+                el("span", [
+                    el("span", today.name, {className: "forecast-data"}),
+                    tempSpan,
+                    el("span", today.forecast.condition, {className: "forecast-data"})
+                ], {className: "condition"}),
+            ], {className: "forecast"}));
 
-            return result
+            upcomingDiv.innerHTML = "";
+            const forecastInfo = el("div", upcoming.forecast.map(renderUpcoming), {className: "forecast-info"});
+            upcomingDiv.appendChild(forecastInfo);
+
+            function renderUpcoming(forecast) {
+                const symbolSpan = el("span", "", {className: "symbol"});
+                symbolSpan.innerHTML = symbols[forecast.condition];
+
+                const tempSpan = el("span", "", {className: "forecast-data"})
+                tempSpan.innerHTML = `${today.forecast.low}${symbols.Degrees}/${today.forecast.high}${symbols.Degrees}`;
+
+                const result = el("span", [
+                    symbolSpan,
+                    tempSpan,
+                    el("span", forecast.condition, {className: "forecast-data"})
+                ], {className: "upcoming"});
+
+                mainDiv.style.display = "block";
+
+                return result
+            }
         }
-        mainDiv.style.display = "block";
     }
 } );
--- 04. JS Applications/03. Asynchronous Programming/01. Forecaster/data.js
+++ 04. JS Applications/03. Asynchronous Programming/01. Forecaster/data.js
@@ -1,30 +1,33 @@
-// 01:55:41
-
 function host(endpoint) {
     return `https://judgetests.firebaseio.com/${endpoint}.json`;
 }
 
 const api = {
     locations: "locations",
-    today: "forecast/today",
-    upcoming: "forecast/upcoming",
+    today: "forecast/today/",
+    upcoming: "forecast/upcoming/",
 };
 
 export async function getCodeAsync(name) {
-    //const response1 = fetch(host(api.locations));
-    // Returns Promise;
-    //const response2 = await (fetch(host(api.locations)));
-    // Returns Response;
-
     const data = await (await fetch(host(api.locations))).json();
 
-    return data.find(x => x.name.toLowerCase() === name.toLowerCase()).code;
+    let result = "";
+
+    try {
+        result = data.find(x => x.name.toLowerCase() === name.toLowerCase()).code;
+    } catch (e) {
+        console.log(e);
+    }
+
+    return result
 }
 
 export async function getTodayAsync(code) {
-    return await (await fetch(host(api.today + code))).json();
+    let response = await (fetch(host(api.today + code)));
+    return await (response.json());
 }
 
-export async function getUpcomingAsync() {
-    return await (await fetch(host(api.upcoming + code))).json();
+export async function getUpcomingAsync(code) {
+    let response = await fetch(host(api.upcoming + code))
+    return await response.json();
 }
