c3f78752ae22dc9dfd3b2df7c0dccb117d2c3a68
pmg23_b.kondev <boriskondev@gmail.com>
2020-07-22 19:22:24 +0300

    22.07.2020

--- 04. JS Applications/06. Routing/TeamManager/scripts/app.js
+++ 04. JS Applications/06. Routing/TeamManager/scripts/app.js
@@ -1,15 +1,35 @@
+import home from "../scripts/controllers/home.js";
+import about from "../scripts/controllers/about.js";
+import login from "../scripts/controllers/login.js";
+import register from "../scripts/controllers/register.js";
+
 $(() => {
     const app = Sammy("#main", function () {
         this.use("Handlebars", "hbs");
 
-        // this === Sammy.Application
-        this.get("index.html", function () {
-            // this === Sammy.EventContext
-            this.render("./templates/register/registerForm.hbs").then(function (html) {
-                // this === Sammy.RenderContext
-                this.swap(html)
-            });
-        });
+        this.userData = {
+            loggedIn: false,
+            hasTeam: false
+        }
+
+        // HOME
+        this.get("index.html", home);
+        this.get("#/home", home);
+        this.get("/", home);
+
+        // ABOUT
+        this.get("#/about", about);
+
+        // LOGIN
+        this.get("#/login", login);
+
+        // REGISTER
+        this.get("#/register", register);
+
+        // Къде държим контролерите?
+        // Как съхраняваме потребителската сесия?
+        // Как подаваме сесията на контролерите?
     });
+
     app.run();
 });
