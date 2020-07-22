8a533873e78c1e6a1bf9704473d44d8399d74063
pmg23_b.kondev <boriskondev@gmail.com>
2020-07-22 13:45:17 +0300

    22.07.2020

--- 04. JS Applications/06. Routing/TeamManager/scripts/app.js
+++ 04. JS Applications/06. Routing/TeamManager/scripts/app.js
@@ -1,1 +1,15 @@
- // TODO
+$(() => {
+    const app = Sammy("#main", function () {
+        this.use("Handlebars", "hbs");
+
+        // this === Sammy.Application
+        this.get("index.html", function () {
+            // this === Sammy.EventContext
+            this.render("./templates/register/registerForm.hbs").then(function (html) {
+                // this === Sammy.RenderContext
+                this.swap(html)
+            });
+        });
+    });
+    app.run();
+});
