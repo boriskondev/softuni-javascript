90e35b306de3bbfad9eab3ffe2b9e721e85cf2ae
pmg23_b.kondev <boriskondev@gmail.com>
2020-07-27 20:25:14 +0300

    27.07.2020

--- 04. JS Applications/07. Workshop SPA/js/app.js
+++ 04. JS Applications/07. Workshop SPA/js/app.js
@@ -1,4 +1,9 @@
-alert("It works!")
+alert("It works!");
+
+import home from "./controllers/home.js"
+import register from "./controllers/register.js"
+import login from "./controllers/login.js"
+import catalog, { details, create, edit } from "./controllers/movies.js"
 
 window.addEventListener("load", () => {
     const app = Sammy("#container", function () {
@@ -7,16 +12,16 @@ window.addEventListener("load", () => {
         this.get("/", home);
         this.get("index.html", home);
         this.get("#/home", home);
+
+        this.get("#/register", register);
+
+        this.get("#/login", login);
+
+        this.get("#/catalog", catalog);
+        this.get("#/details/:id", details);
+        this.get("#/create", create);
+        this.get("#/edit/:id", edit);
     })
     
     app.run();
-    
-    async function home() {
-        this.partials = {
-            header: await this.load("./templates/common/header.hbs"),
-            footer: await this.load("./templates/common/footer.hbs")
-        };
-        this.partial("./templates/home.hbs")
-
-    }
 })
